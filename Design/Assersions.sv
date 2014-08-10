658e061b59e02ad67a785255a57b38bb4171dcd6
bhunt2 <bhunt2@pdx.edu>
2014-08-10 16:35:24 -0700

    updates to assertion notes

--- Design/Assersions.sv
+++ Design/Assersions.sv
@@ -7,10 +7,10 @@ property Activate
 endproperty
 
 // Input data mask
-//  
-property InputMask
-**********************
-endproperty
+//  ** This does not need implementing **
+//property InputMask
+//**********************
+//endproperty
 
 // Reset
 //  The reset is active low and asynchronous.  System must return to default state
@@ -22,9 +22,10 @@ endproperty
 // Data Strobe
 //  This is the signal that transmits data.  It is an inout signal because it is an
 //  edge aligned output with read data, and a centered input with write data.
-property DataStrobe
-	*************
-endproperty
+//  ** This does not need an assertion.  It was verified using timing diagram **
+//property DataStrobe
+//	*************
+//endproperty
 
 // Termination Data Strobe
 //  This is applicable to x8 DRAMs only.  When enabled in Mode Register A11 = 1
@@ -33,16 +34,17 @@ endproperty
 //  MR1, DM/DBI/TDQS will provide the data mask function or Data Bus Inversion
 //  depending on MR5.  A11, 12, 10 and TDQS_c are not used.  x4/x16 DRAMs must
 //  disable the TDQS function via mode register A11 = 0 in MR1.
-property TerminateDataStrobe
-	if (DRAMType === x8)
-		MR1A11 == 1 |-> ******
-	else
-		MR1A11 == 0;
-endproperty
+//  ** This is set to disabled.  No need to implement. **
+//property TerminateDataStrobe
+//	if (DRAMType === x8)
+//		MR1A11 == 1 |-> ******
+//	else
+//		MR1A11 == 0;
+//endproperty
 
 // Write Recovery and Read to Precharge (cycles)
-//  Can be set in MR0 A11, A10, and A9.  The table for these settins are on
-//  page 15 of the JEDEC Guide.
+//  Can be set in MR0 A11, A10, and A9.  The table for these settings are on
+//  page 14 of the JEDEC Guide.
 //  ** Is this going to be utilized? **
 property
 	
@@ -70,9 +72,8 @@ endproperty
 //  and explained in Section 4.15 of the JEDEC guide.
 //  ** Is this going to be utilized? **
 //  ** Are we going to parameterize for simulation of different speeds? **
-property
+// This is being set to disabled
 
-endproperty
 
 // ** What commands are being implemented **
 //  I will write properties for each implemented command to ensure that each
@@ -86,6 +87,7 @@ endproperty
 //  a burst is determined by the burst length, burst type, and the starting column
 //  address as shown in Table 18 on page 26 of the JEDEC Guide.
 //  ** Is this going to be implemented or will it be static? **
+//    This is the only mode register bit that will change in our simulation
 property
 	
 endproperty
@@ -93,16 +95,23 @@ endproperty
 // Write Leveling
 //  The settings for Write Leveling can be found in section 4.7.
 //  ** Is this being implemented? **
+//     This is not implemented
 
 // Refresh
 //  4.9
 
-// Write to Write tCCD Timing
+//******************************************************************************
+// More defined timing parameters for the timing settings below can be found
+//  in Section 12.3
+
+// Write to Write tCCD Timing (CAS_n to CAS_n)
 //  Timing diagram found in Section 4.19
 //  tCCD_S (short) corresponds to consecutive writes to different Bank Group
 //   - Requires 4 clock cycles from write to write
 //  tCCD_L (long) corresponds to consecutive writes to the same Bank Group
 //   - Requires 6 clock cycles from write to write
+//     ** Note: Figure 55 shows 6 clocks but Table 101 shows 5 clocks
+//       6 was used in our implementation
 
 
 // Read to Read tCCD Timing
@@ -114,7 +123,7 @@ endproperty
 
 
 // Active to Active tRRD Timing
-//  Timing diagram found in Section 4.19
+//  Timing diagram found in Section 4.19, Figure 57
 //  tRRD_S (short) corresponds to consecutive ACTIVATE commands to different Bank Group
 //   - Requires 4 clock cycles from ACTIVATE to ACTIVATE
 //  tRRD_L (long) corresponds to consecutive ACTIVATE commands to different Banks
@@ -122,6 +131,10 @@ endproperty
 //   - Requires 6 clock cycles from ACTIVATE to ACTIVATE
 
 
-// Four Active Window
-//  This is referenced in Figure 58 found in Section 4.19 but need more information
-//  about it.
+// Four Activate Window
+//  This is the timing requirement between four consecutive activate ACTIVATE commands.
+//  The timing diagram can be found in Section 4.19, Figure 58 and more specific
+//  information in Table 101 on pg. 189(197) of the JEDEC Guide.
+//   This was ignored in our implementation
+
+
