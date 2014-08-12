419b8d8a0867dae687703f1df980747543a23aff
unknown <bhunt2@FAB16.ds.cecs.pdx.edu>
2014-08-12 11:32:23 -0700

    trying to fix a bad commit

--- src/Rand_Stimulus.sv
+++ src/Rand_Stimulus.sv
@@ -17,14 +17,13 @@ parameter num_op = 100;
 
 // Use class for randomization
 class Packet;
-
 // Random variables
 randc bit [63:0] data_r;
 randc bit [31:0] addr_r;
 randc bit [1:0] op_r;
 // Limit values for op to R/W (01 or 10)
 constraint c {op_r >= 2'b01;
-			  op_r <= 2'b10;}
+	      op_r <= 2'b10;}
 endclass
 
 //class Gen_Packet
@@ -32,32 +31,32 @@ class Gen_Packet;
     rand Packet Packet_array[];
     bit [31:0] addr_queue[$];
     constraint rw {
-        foreach (Packet_array[i])
-            Packet_array[i].op_r dist {READ:= 50, WRITE:= 50};
-    }          
+         foreach (Packet_array[i])
+             Packet_array[i].op_r dist {READ:= 50, WRITE:= 50};
+             }          
     
     function void post_randomize;
-        foreach (Packet_array[i]) begin
-            if ((i === 0) || (addr_queue.size === 0))
-				Packet_array[i].op_r = WRITE;
-            if(Packet_array[i].op_r === WRITE) 
-				addr_queue 					= {Packet_array[i].addr_r, addr_queue};   
-            if ((i > 0) && (Packet_array [i].op_r  === READ ))
-                Packet_array[i].addr_r   	= addr_queue.pop_back;
-        end             
+         foreach (Packet_array[i]) begin
+            if ((i == 0) || (addr_queue.size == 0))
+               Packet_array[i].op_r = WRITE;
+            if(Packet_array[i].op_r == WRITE) 
+               addr_queue = {Packet_array[i].addr_r, addr_queue};   
+            if ((i > 0) && (Packet_array [i].op_r   == READ ))
+                Packet_array[i].addr_r   = addr_queue.pop_back;
+         end             
     endfunction
     
     function new();
-		addr_queue.delete;
-		Packet_array = new[num_op];
-		foreach (Packet_array[i])
-			Packet_array[i] = new();
+       addr_queue.delete;
+       Packet_array = new[num_op];
+       foreach (Packet_array[i])
+          Packet_array[i] = new();
     endfunction
     
     function void print_all() ;
     foreach (Packet_array[i])
-		$display ("addr = %h, data = %h, rw = %h", Packet_array[i].addr_r, 
-			Packet_array[i].data_r, Packet_array[i].op_r);
+       $display ("addr = %h, data = %h, rw = %h", Packet_array[i].addr_r, 
+          Packet_array[i].data_r, Packet_array[i].op_r);
     endfunction      
                 
 endclass
@@ -76,11 +75,11 @@ initial begin
 	p.print_all();
 	
 	foreach (p.Packet_array[i]) begin
-		Stim_st.data_wr       = p.Packet_array[i].data_r;
-		Stim_st.physical_addr = p.Packet_array[i].addr_r;
-		Stim_st.rw            = p.Packet_array[i].op_r;
+	   Stim_st.data_wr       = p.Packet_array[i].data_r;
+	   Stim_st.physical_addr = p.Packet_array[i].addr_r;
+	   Stim_st.rw            = p.Packet_array[i].op_r;
 
-		su.push_front(Stim_st);
+ 	   su.push_front(Stim_st);
 	end
 
 	wait (test);
@@ -93,10 +92,11 @@ end
 
 always_ff @ (intf.clock_t)
 begin
-    if ((!tb_intf.dev_busy) && (su.size >0 ) && (tb_intf.next_cmd))
-       tb_intf.act_cmd 		<= 1'b1;
+    if ((!tb_intf.dev_busy) &&(su.size >0 ) && (tb_intf.next_cmd))
+       tb_intf.act_cmd <= 1'b1;
     else   
-       tb_intf.act_cmd 		<= 1'b0;
+       tb_intf.act_cmd <= 1'b0;
+
 end
 
 endmodule 
--- src/ddr_controller.sv
+++ src/ddr_controller.sv
@@ -30,114 +30,118 @@ logic clear_update_counter, update_done;
 assign  tb_intf.dev_busy = (ctrl_state == CTRL_RW)? 1'b0: 1'b1;
   
 //fsm ddr controller
-always_ff @(posedge intf.clock_t, negedge intf.reset_n) begin
-	if (!intf.reset_n) 
-		ctrl_state <= CTRL_IDLE;
-	else 
+always_ff @(posedge intf.clock_t, negedge intf.reset_n)
+begin
+  if (!intf.reset_n) 
+     ctrl_state <= CTRL_IDLE;
+  else 
      ctrl_state <= ctrl_next_state;
 end
    
    
 //next state generate logic
 //
-always_comb begin
-	if (!intf.reset_n)
-		ctrl_next_state <= CTRL_IDLE;
+always_comb
+begin
+   if (!intf.reset_n)
+      ctrl_next_state <= CTRL_IDLE;
       
-	case (ctrl_state)
-	CTRL_IDLE: begin
-		clear_counter            		<= 1'b1;
-		ctrl_intf.mrs_update_rdy 		<= 1'b0;
-		clear_update_counter     		<= 1'b1;
-		ctrl_intf.rw_proc        		<= 1'b0;
-		if (intf.reset_n)
-			ctrl_next_state       		<= CTRL_INIT;
-	end   
+   case (ctrl_state)
+   CTRL_IDLE: begin
+      clear_counter            <= 1'b1;
+      ctrl_intf.mrs_update_rdy <= 1'b0;
+      clear_update_counter     <= 1'b1;
+      ctrl_intf.rw_proc        <= 1'b0;
+      if (intf.reset_n)
+         ctrl_next_state    <= CTRL_INIT;
+      end   
             
    CTRL_INIT: begin  
-		clear_counter 					<= 1'b1;
-		if(ctrl_intf.config_done) begin
-			ctrl_next_state  			<= CTRL_RW;
-			ctrl_intf.rw_proc			<= 1'b1;
-		end   
-	end
+      clear_counter <= 1'b1;
+      if(ctrl_intf.config_done) begin
+         ctrl_next_state  <= CTRL_RW;
+         ctrl_intf.rw_proc<= 1'b1;
+      end   
+   end
            
    CTRL_RW: begin
-		clear_counter 					<= 1'b0;    // assume refresh occurs only rw, and update
-		if ((mrs_update) || (refresh_almost)) begin
-			ctrl_next_state    			<= CTRL_WAIT;    
-			ctrl_intf.rw_proc  			<= 1'b0;                    
-		end
-	end
+      clear_counter <= 1'b0;    // assume refresh occurs only rw, and update
+      if ((mrs_update) || (refresh_almost)) begin
+         ctrl_next_state    <= CTRL_WAIT;    
+         ctrl_intf.rw_proc  <= 1'b0;                    
+      end
+   end
            
    CTRL_WAIT: begin
-		if (ctrl_intf.rw_idle) begin
-			if (refresh_almost)
-				ctrl_next_state 			<= CTRL_REFRESH;
-			else begin
-				ctrl_intf.mrs_update_rdy	<= 1'b1;
-				//copy MR0 from burst_conf and update burst length
-				ctrl_intf.mrs_update_cmd	<= {ctrl_intf.mr0[MRS_WIDTH -1:2],mrs_bl};
-				ctrl_next_state <= CTRL_UPDATE;  
-			end
-		end
+      if (ctrl_intf.rw_idle) begin
+         if (refresh_almost)
+            ctrl_next_state <= CTRL_REFRESH;
+         else begin
+            ctrl_intf.mrs_update_rdy  <= 1'b1;
+            //copy MR0 from burst_conf and update burst length
+            ctrl_intf.mrs_update_cmd <= {ctrl_intf.mr0[MRS_WIDTH -1:2],mrs_bl};
+            ctrl_next_state <= CTRL_UPDATE;  
+         end
+      end
    end
                
    CTRL_UPDATE: begin
-		ctrl_intf.mrs_update_rdy  		<= 1'b0;
-		clear_update_counter   			<= 1'b0;
-		if (update_done) begin
-			ctrl_next_state    			<= CTRL_RW;
-			clear_update_counter 		<= 1'b1;
-		end
-	end
+      ctrl_intf.mrs_update_rdy       <= 1'b0;
+      clear_update_counter           <= 1'b0;
+      if (update_done) begin
+         ctrl_next_state    <= CTRL_RW;
+         clear_update_counter <= 1'b1;
+      end
+   end
            
    CTRL_REFRESH: begin
-		if (refresh_done) begin
-			ctrl_next_state   			<= CTRL_RW;
-			ctrl_intf.rw_proc 			<= 1'b1;
-		end   
-    end
+      if (refresh_done) begin
+         ctrl_next_state   <= CTRL_RW;
+         ctrl_intf.rw_proc <= 1'b1;
+      end   
+      end
              
-	default: ctrl_next_state 			<= CTRL_IDLE;
+   default: ctrl_next_state <= CTRL_IDLE;
           
-	endcase
+   endcase
 end             
               
       // simple counter 
-    always_ff @(posedge intf.clock_t) begin
-		if(clear_counter === 1'b1) begin
-			refresh_counter      		<= 0;
-			refresh_almost       		<= 1'b0;
-			ctrl_intf.refresh_rdy		<= 1'b0;
-			refresh_done         		<= 1'b0;
-		end
-		else begin
-			refresh_counter 			<= refresh_counter + 1;
-			if (refresh_counter == tREF - 100) 
-				refresh_almost 			<= 1'b1;
-			else if (refresh_counter == tREF) begin
-				refresh_almost 			<= 1'b0;
-				ctrl_intf.refresh_rdy 	<= 1'b1;
-			end   
-			else if (refresh_counter == tREF + 1)
-				ctrl_intf.refresh_rdy 	<= 1'b0;   
-			else if (refresh_counter == tREF + tRC)
-				refresh_done    		<= 1'b1;
-		end        
-	end		
+    always_ff @(posedge intf.clock_t)
+    begin
+       if(clear_counter == 1'b1) begin
+          refresh_counter      <= 0;
+          refresh_almost       <= 1'b0;
+          ctrl_intf.refresh_rdy<= 1'b0;
+          refresh_done         <= 1'b0;
+          end
+       else begin
+          refresh_counter <= refresh_counter + 1;
+          if (refresh_counter == tREF - 100) 
+             refresh_almost <= 1'b1;
+          else if (refresh_counter == tREF) begin
+             refresh_almost <= 1'b0;
+             ctrl_intf.refresh_rdy <= 1'b1;
+          end   
+          else if (refresh_counter == tREF + 1)
+             ctrl_intf.refresh_rdy <= 1'b0;   
+          else if (refresh_counter == tREF + tRC)
+             refresh_done    <= 1'b1;
+          end        
+     end 
       
-    // simple act_counter 
-    always_ff @(posedge intf.clock_t) begin
-		if(clear_update_counter === 1'b1) begin
-			update_done    				<= 1'b0;
-			update_counter 				<= 0;
-        end
-		else begin
-			update_counter <= update_counter + 1;
-			if (update_counter === tMOD) 
-				update_done  <= 1'b1;
-		end                            
+       // simple act_counter 
+    always_ff @(posedge intf.clock_t)
+    begin
+       if(clear_update_counter == 1'b1) begin
+          update_done    <= 1'b0;
+          update_counter <= 0;
+          end
+       else begin
+          update_counter <= update_counter + 1;
+          if (update_counter == tMOD) 
+             update_done  <= 1'b1;
+          end                            
     end
 
 endmodule
